5e2e98495dc2d9eec244804189b372f6fc41bb74
dl1998 <dima.leschenko@mail.ru>
2018-11-06 18:00:20 +0100

    Added Content In Overview/Fixed Footer Bug (For Some Resolution)

--- assets/js/deep_tour.js
+++ assets/js/deep_tour.js
@@ -1,4 +1,4 @@
 $(".dropdown-menu a").click(function(){
-    $(this).parents(".dropdown").find('.dropdown-toggle').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.dropdown-toggle').html("Sorted By: " + $(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.dropdown-toggle').val($(this).data('value'));
 });
