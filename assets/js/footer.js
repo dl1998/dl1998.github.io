1fe6142bac9dade362eb4422471678a64808bbc3
dl1998 <dima.leschenko@mail.ru>
2018-11-17 01:47:53 +0100

    Reverse To Sync Load Header

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,3 +1,4 @@
+jQuery.ajaxSetup({async:false});
 $.get("footer.html", function (data) {
     $('footer').replaceWith(data);
 });
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,4 @@
+jQuery.ajaxSetup({async:false});
 $.get("navbar.html", function (data) {
     $('nav').replaceWith(data);
 });
